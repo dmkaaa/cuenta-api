c15efa2fd8c26d7f6cd636bc6c449e43d37062f1
dmitri <dmitri.batyrjev@gmail.com>
2024-01-22 21:13:22 +0200

    Init

---
+++ src/main/kotlin/com/github/dmkaaa/cuenta/CuentaApplication.kt
@@ -0,0 +1,11 @@
+package com.github.dmkaaa.cuenta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CuentaApplication
+
+fun main(args: Array<String>) {
+	runApplication<CuentaApplication>(*args)
+}
